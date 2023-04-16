ca0ed180f6ae79bc3b98438ef0e1f0c478ac9f59
Naman Jain <namanviber@gmail.com>
2023-04-16 16:24:06 +0530

    commit1

---
+++ src/app.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+let favicon = require('serve-favicon');
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.of('/stream').on('connection', stream,()=>
+{
+    console.log('socket connected');
+});
+
+server.listen(3000 ,"0.0.0.0", function () {
+    console.log("Server Connected");
+});
